048f5bf60e75ffe39bfd5b295390e7b61efec967
Caio Lucas <lcaio1281@gmail.com>
2023-05-06 13:24:44 -0300

    config path mapping with babel

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
